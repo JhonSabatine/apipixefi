ecb2b927c8ebd620b0e3d7daf4457d748dd421fa
Jonathan Sabatine <jonathandouglas7@hotmail.com>
2024-05-02 11:18:05 -0300

    refatorando para novo arquivo

--- src/server.js
+++ src/server.js
@@ -2,54 +2,18 @@ if (process.env.NODE_ENV !== 'production') {
  require('dotenv').config()
 }
 
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
-const { url } = require('inspector');
 const express = require('express');
-
-const cert = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.EFI_CERT}`)
-);
-
-const agent = new https.Agent({
-    pfx: cert,
-    passphrase: ''
-});
-
-const credentials = Buffer.from(`${process.env.EFI_CLIENT_ID}:${process.env.EFI_CLIENT_SECRET}`
-).toString('base64');
+const efiRequest = require('./apis/efi');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
+
+const reqEFAlready = efiRequest();
+
 app.get('/', async (req, res) => {
-    const authResponse = await axios({
-        method: 'POST',
-        url: `${process.env.EFI_ENDPOINT}/oauth/token`,
-        headers: {
-            Authorization: `Basic ${credentials}`,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: agent,
-        data: {
-            grant_type: 'client_credentials'
-        }
-    });
-
-    const accessToken = authResponse.data?.access_token;
-
-    const reqEF = axios.create({
-        baseURL: process.env.EFI_ENDPOINT,
-        httpsAgent: agent,
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-        }
-    });
-
+    const reqEF = await reqEFAlready;
     const dataCob = {
         calendario: {
             expiracao: 3600
